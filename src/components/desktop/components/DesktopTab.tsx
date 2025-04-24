247004079ec1babc52ea468b82c8102ff122f4e6
Seth <8thseth@gmail.com>
2025-04-24 19:42:42 -0300

    fix: remove all translates

--- src/components/desktop/components/DesktopTab.tsx
+++ src/components/desktop/components/DesktopTab.tsx
@@ -9,15 +9,17 @@ export default function DesktopTab({
 }: Props) {
 
   return (
-    <div
-      id="tab_home"
-      className={`max-w-[480px] max-md:h-fit md:max-h-[350px] h-full w-full z-[2] top-1/2 md:top-1/3 left-1/2 transform -translate-x-1/2 -translate-y-1/2 md:-translate-y-1/3 absolute md:bg-violet-100 dark:md:bg-slate-700 flex flex-col rounded-lg border-3 border-[#00000000] md:border-slate-800 select-none`}
-    >
-      <div className={`font-abz opacity-0 md:opacity-100 max-md:h-0 max-md:py-0 transition-all bg-violet-400 duration-[0ms] dark:bg-indigo-400 flex border-b-3 border-slate-800 text-slate-800 dark:text-slate-800 px-8 py-3 rounded-t-md tracking-wider w-full items-center justify-between`}>
-        <span className="opacity-0 md:opacity-100 transition-opacity">home</span>
-      </div>
-      <div className={`w-full h-full [&>*]:p-8 [&>*]:pt-10 select-text md:bg-violet-100 dark:md:bg-slate-700 text-slate-800 dark:text-slate-400 rounded-b-md `}>
-        {children}
+    <div className="bg-red-200 max-w-5xl w-full flex justify-center items-center h-[calc(var(--dvh)-36px)] md:h-[calc(var(--dvh)-7rem)] transition-[height]">
+      <div
+        id="tab_home"
+        className={`max-w-[480px] max-md:h-fit md:max-h-[350px] h-full w-full z-[2] absolute md:bg-violet-100 dark:md:bg-slate-700 flex flex-col rounded-lg border-3 border-[#00000000] md:border-slate-800 select-none`}
+      >
+        <div className={`font-abz opacity-0 md:opacity-100 max-md:h-0 max-md:py-0 transition-all bg-violet-400 duration-[0ms] dark:bg-indigo-400 flex border-b-3 border-slate-800 text-slate-800 dark:text-slate-800 px-8 py-3 rounded-t-md tracking-wider w-full items-center justify-between`}>
+          <span className="opacity-0 md:opacity-100 transition-opacity">home</span>
+        </div>
+        <div className={`w-full h-full [&>*]:p-8 [&>*]:pt-10 select-text md:bg-violet-100 dark:md:bg-slate-700 text-slate-800 dark:text-slate-400 rounded-b-md `}>
+          {children}
+        </div>
       </div>
     </div>
   );
